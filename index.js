43086665961ac2bf971ff5a86bc1cdb3258af303
Md Mehedi Ahsan <ahsanmehedi041@gmail.com>
2022-10-26 01:16:04 +0600

    courses data added

--- index.js
+++ index.js
@@ -5,8 +5,20 @@ const port = process.env.PORT || 5000;
 
 app.use(cors());
 
+const courses = require('./data/courses.json');
+
 app.get('/', (req, res) => {
     res.send('Courses api is running');
+})
+
+app.get('/courses', (req, res) => {
+    res.send(courses);
+})
+
+app.get('/course/:id', (req, res) => {
+    const id = req.params.id;
+    const selectedCourse = courses.find(c => c.id === id);
+    res.send(selectedCourse);
 })
 
 app.listen(port, () => {
